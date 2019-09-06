b7c481e621eb899a8cdd7b88bbc8df44344bde26
gianniresende <gianniresende@gmail.com>
2019-09-05 21:17:34 -0300

    add resouces for backend

--- Back_End/src/server.js
+++ Back_End/src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes/routes');
 const startBD = require('./config/BD/mongo');
+const cors = require('cors');
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 server.listen(3333);
